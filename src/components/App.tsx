655bed9dbb90b59ffacd67b617ba8f63cdafe786
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-12-23 18:34:29 +0900

    ファイル, package.jsonの内容の整理

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import {useForm} from 'react-hook-form';
-import MarkdownViewer from './MarkdownViewer';
-import '../styles/components/app.scss';
+import React, { useState } from 'react'
+import MarkdownViewer from './MarkdownViewer'
 
 const App = () => {
-  const {register, watch} = useForm();
-  const watchMarkdownText = watch('markdown-text', '');
+  const [markdownText, setMarkdownText] = useState('')
 
   return (
-    <div className="app">
-      <div className="app__container">
-        <div className="app__heading app__heading--editor">エディタ</div>
+    <div className='app'>
+      <div className='app__container'>
+        <div className='app__heading app__heading--editor'>エディタ</div>
         <textarea
-          className="app__editor"
-          placeholder="マークダウンが利用できます。"
-          {...register('markdown-text')}
+          className='app__editor'
+          placeholder='マークダウンが利用できます。'
+          value={markdownText}
+          onChange={(event) => setMarkdownText(event.target.value)}
         />
       </div>
-      <div className="app__container">
-        <div className="app__heading app__heading--preview">プレビュー</div>
-        <MarkdownViewer markdownText={watchMarkdownText} />
+      <div className='app__container'>
+        <div className='app__heading app__heading--preview'>プレビュー</div>
+        <MarkdownViewer markdownText={markdownText} />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
