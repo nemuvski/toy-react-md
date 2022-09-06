4dc6f9c183fafcf95892d0d3bb870ebfb61d8d15
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-09-06 12:15:29 +0900

    利用パッケージアップデート (React v18対応)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
 import './styles/index.scss'
+const container = document.getElementById('root')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+if (!container) throw new Error('Oops, root element is missing.')
+
+const root = createRoot(container)
+root.render(<App />)
